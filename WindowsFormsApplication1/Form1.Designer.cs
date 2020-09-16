c7fbd97ddd608003546a35235f2132ff3af4be74
Celiwi <58071688+Celiwi@users.noreply.github.com>
2020-09-16 14:35:29 +0200

    version 2
    Ejercicio 1 ya terminado

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -34,12 +34,29 @@
             this.ListaMaxYMin = new System.Windows.Forms.ListBox();
             this.BtnPosiciones = new System.Windows.Forms.Button();
             this.ListaPosicion = new System.Windows.Forms.ListBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnAritmetica = new System.Windows.Forms.Button();
             this.ListaAritmetica = new System.Windows.Forms.ListBox();
-            this.BotónOrdenar = new System.Windows.Forms.Button();
+            this.btnOrden = new System.Windows.Forms.Button();
             this.ListaOrden = new System.Windows.Forms.ListBox();
-            this.BotónBuscar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
             this.ListaBuscar = new System.Windows.Forms.ListBox();
+            this.TextoEscribir = new System.Windows.Forms.TextBox();
+            this.btnInvertir = new System.Windows.Forms.Button();
+            this.ListaInvertir = new System.Windows.Forms.ListBox();
+            this.btnReemplazar = new System.Windows.Forms.Button();
+            this.ListaReemplazar = new System.Windows.Forms.ListBox();
+            this.btnAleatorioRepeticiones = new System.Windows.Forms.Button();
+            this.ListaNoRepeticiones = new System.Windows.Forms.ListBox();
+            this.btnContar = new System.Windows.Forms.Button();
+            this.ListaContar = new System.Windows.Forms.ListBox();
+            this.btnOrden2 = new System.Windows.Forms.Button();
+            this.ListaOrden2 = new System.Windows.Forms.ListBox();
+            this.btnBuscarReemplazar = new System.Windows.Forms.Button();
+            this.TextoEscribir2 = new System.Windows.Forms.TextBox();
+            this.ListaBuscarReemplazar = new System.Windows.Forms.ListBox();
+            this.btnBuscarContar = new System.Windows.Forms.Button();
+            this.TextoEscribir3 = new System.Windows.Forms.TextBox();
+            this.ListaBuscarContar = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // BtnAleatorio
@@ -97,15 +114,15 @@
             this.ListaPosicion.Size = new System.Drawing.Size(120, 95);
             this.ListaPosicion.TabIndex = 5;
             // 
-            // button1
+            // btnAritmetica
             // 
-            this.button1.Location = new System.Drawing.Point(473, 24);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Botón4";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnAritmetica.Location = new System.Drawing.Point(473, 24);
+            this.btnAritmetica.Name = "btnAritmetica";
+            this.btnAritmetica.Size = new System.Drawing.Size(75, 23);
+            this.btnAritmetica.TabIndex = 6;
+            this.btnAritmetica.Text = "Botón4";
+            this.btnAritmetica.UseVisualStyleBackColor = true;
+            this.btnAritmetica.Click += new System.EventHandler(this.button1_Click);
             // 
             // ListaAritmetica
             // 
@@ -115,15 +132,15 @@
             this.ListaAritmetica.Size = new System.Drawing.Size(145, 95);
             this.ListaAritmetica.TabIndex = 7;
             // 
-            // BotónOrdenar
+            // btnOrden
             // 
-            this.BotónOrdenar.Location = new System.Drawing.Point(624, 23);
-            this.BotónOrdenar.Name = "BotónOrdenar";
-            this.BotónOrdenar.Size = new System.Drawing.Size(75, 23);
-            this.BotónOrdenar.TabIndex = 8;
-            this.BotónOrdenar.Text = "Botón5";
-            this.BotónOrdenar.UseVisualStyleBackColor = true;
-            this.BotónOrdenar.Click += new System.EventHandler(this.BotónOrdenar_Click);
+            this.btnOrden.Location = new System.Drawing.Point(624, 23);
+            this.btnOrden.Name = "btnOrden";
+            this.btnOrden.Size = new System.Drawing.Size(75, 23);
+            this.btnOrden.TabIndex = 8;
+            this.btnOrden.Text = "Botón5";
+            this.btnOrden.UseVisualStyleBackColor = true;
+            this.btnOrden.Click += new System.EventHandler(this.BotónOrdenar_Click);
             // 
             // ListaOrden
             // 
@@ -133,34 +150,199 @@
             this.ListaOrden.Size = new System.Drawing.Size(120, 95);
             this.ListaOrden.TabIndex = 9;
             // 
-            // BotónBuscar
+            // btnBuscar
             // 
-            this.BotónBuscar.Location = new System.Drawing.Point(771, 23);
-            this.BotónBuscar.Name = "BotónBuscar";
-            this.BotónBuscar.Size = new System.Drawing.Size(75, 23);
-            this.BotónBuscar.TabIndex = 10;
-            this.BotónBuscar.Text = "Botón 6";
-            this.BotónBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Location = new System.Drawing.Point(35, 203);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 10;
+            this.btnBuscar.Text = "Botón 6";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.BotónBuscar_Click);
             // 
             // ListaBuscar
             // 
             this.ListaBuscar.FormattingEnabled = true;
-            this.ListaBuscar.Location = new System.Drawing.Point(771, 76);
+            this.ListaBuscar.Location = new System.Drawing.Point(35, 270);
             this.ListaBuscar.Name = "ListaBuscar";
             this.ListaBuscar.Size = new System.Drawing.Size(120, 95);
             this.ListaBuscar.TabIndex = 11;
+            // 
+            // TextoEscribir
+            // 
+            this.TextoEscribir.Location = new System.Drawing.Point(35, 233);
+            this.TextoEscribir.Name = "TextoEscribir";
+            this.TextoEscribir.Size = new System.Drawing.Size(100, 20);
+            this.TextoEscribir.TabIndex = 12;
+            // 
+            // btnInvertir
+            // 
+            this.btnInvertir.Location = new System.Drawing.Point(182, 203);
+            this.btnInvertir.Name = "btnInvertir";
+            this.btnInvertir.Size = new System.Drawing.Size(75, 23);
+            this.btnInvertir.TabIndex = 13;
+            this.btnInvertir.Text = "Botón 7";
+            this.btnInvertir.UseVisualStyleBackColor = true;
+            this.btnInvertir.Click += new System.EventHandler(this.BotonInvertir_Click);
+            // 
+            // ListaInvertir
+            // 
+            this.ListaInvertir.FormattingEnabled = true;
+            this.ListaInvertir.Location = new System.Drawing.Point(182, 270);
+            this.ListaInvertir.Name = "ListaInvertir";
+            this.ListaInvertir.Size = new System.Drawing.Size(120, 95);
+            this.ListaInvertir.TabIndex = 14;
+            // 
+            // btnReemplazar
+            // 
+            this.btnReemplazar.Location = new System.Drawing.Point(331, 203);
+            this.btnReemplazar.Name = "btnReemplazar";
+            this.btnReemplazar.Size = new System.Drawing.Size(75, 23);
+            this.btnReemplazar.TabIndex = 15;
+            this.btnReemplazar.Text = "Botón 8";
+            this.btnReemplazar.UseVisualStyleBackColor = true;
+            this.btnReemplazar.Click += new System.EventHandler(this.btnReemplazar_Click);
+            // 
+            // ListaReemplazar
+            // 
+            this.ListaReemplazar.FormattingEnabled = true;
+            this.ListaReemplazar.Location = new System.Drawing.Point(331, 270);
+            this.ListaReemplazar.Name = "ListaReemplazar";
+            this.ListaReemplazar.Size = new System.Drawing.Size(120, 95);
+            this.ListaReemplazar.TabIndex = 16;
+            // 
+            // btnAleatorioRepeticiones
+            // 
+            this.btnAleatorioRepeticiones.Location = new System.Drawing.Point(473, 202);
+            this.btnAleatorioRepeticiones.Name = "btnAleatorioRepeticiones";
+            this.btnAleatorioRepeticiones.Size = new System.Drawing.Size(75, 23);
+            this.btnAleatorioRepeticiones.TabIndex = 17;
+            this.btnAleatorioRepeticiones.Text = "Botón 9";
+            this.btnAleatorioRepeticiones.UseVisualStyleBackColor = true;
+            this.btnAleatorioRepeticiones.Click += new System.EventHandler(this.btnAleatorioRepeticiones_Click);
+            // 
+            // ListaNoRepeticiones
+            // 
+            this.ListaNoRepeticiones.FormattingEnabled = true;
+            this.ListaNoRepeticiones.Location = new System.Drawing.Point(473, 270);
+            this.ListaNoRepeticiones.Name = "ListaNoRepeticiones";
+            this.ListaNoRepeticiones.Size = new System.Drawing.Size(120, 95);
+            this.ListaNoRepeticiones.TabIndex = 18;
+            // 
+            // btnContar
+            // 
+            this.btnContar.Location = new System.Drawing.Point(624, 202);
+            this.btnContar.Name = "btnContar";
+            this.btnContar.Size = new System.Drawing.Size(75, 23);
+            this.btnContar.TabIndex = 19;
+            this.btnContar.Text = "Botón 10";
+            this.btnContar.UseVisualStyleBackColor = true;
+            this.btnContar.Click += new System.EventHandler(this.btnContar_Click);
+            // 
+            // ListaContar
+            // 
+            this.ListaContar.FormattingEnabled = true;
+            this.ListaContar.Location = new System.Drawing.Point(624, 270);
+            this.ListaContar.Name = "ListaContar";
+            this.ListaContar.Size = new System.Drawing.Size(171, 95);
+            this.ListaContar.TabIndex = 20;
+            // 
+            // btnOrden2
+            // 
+            this.btnOrden2.Location = new System.Drawing.Point(35, 391);
+            this.btnOrden2.Name = "btnOrden2";
+            this.btnOrden2.Size = new System.Drawing.Size(75, 23);
+            this.btnOrden2.TabIndex = 21;
+            this.btnOrden2.Text = "Botón 11";
+            this.btnOrden2.UseVisualStyleBackColor = true;
+            this.btnOrden2.Click += new System.EventHandler(this.btnOrdenar2_Click);
+            // 
+            // ListaOrden2
+            // 
+            this.ListaOrden2.FormattingEnabled = true;
+            this.ListaOrden2.Location = new System.Drawing.Point(35, 439);
+            this.ListaOrden2.Name = "ListaOrden2";
+            this.ListaOrden2.Size = new System.Drawing.Size(120, 95);
+            this.ListaOrden2.TabIndex = 22;
+            // 
+            // btnBuscarReemplazar
+            // 
+            this.btnBuscarReemplazar.Location = new System.Drawing.Point(182, 391);
+            this.btnBuscarReemplazar.Name = "btnBuscarReemplazar";
+            this.btnBuscarReemplazar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscarReemplazar.TabIndex = 23;
+            this.btnBuscarReemplazar.Text = "Botón 12";
+            this.btnBuscarReemplazar.UseVisualStyleBackColor = true;
+            this.btnBuscarReemplazar.Click += new System.EventHandler(this.btnBuscarReemplazar_Click);
+            // 
+            // TextoEscribir2
+            // 
+            this.TextoEscribir2.Location = new System.Drawing.Point(182, 421);
+            this.TextoEscribir2.Name = "TextoEscribir2";
+            this.TextoEscribir2.Size = new System.Drawing.Size(100, 20);
+            this.TextoEscribir2.TabIndex = 24;
+            // 
+            // ListaBuscarReemplazar
+            // 
+            this.ListaBuscarReemplazar.FormattingEnabled = true;
+            this.ListaBuscarReemplazar.Location = new System.Drawing.Point(182, 448);
+            this.ListaBuscarReemplazar.Name = "ListaBuscarReemplazar";
+            this.ListaBuscarReemplazar.Size = new System.Drawing.Size(120, 82);
+            this.ListaBuscarReemplazar.TabIndex = 25;
+            // 
+            // btnBuscarContar
+            // 
+            this.btnBuscarContar.Location = new System.Drawing.Point(331, 390);
+            this.btnBuscarContar.Name = "btnBuscarContar";
+            this.btnBuscarContar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscarContar.TabIndex = 26;
+            this.btnBuscarContar.Text = "Botón 13";
+            this.btnBuscarContar.UseVisualStyleBackColor = true;
+            this.btnBuscarContar.Click += new System.EventHandler(this.btnBuscarContar_Click);
+            // 
+            // TextoEscribir3
+            // 
+            this.TextoEscribir3.Location = new System.Drawing.Point(331, 420);
+            this.TextoEscribir3.Name = "TextoEscribir3";
+            this.TextoEscribir3.Size = new System.Drawing.Size(100, 20);
+            this.TextoEscribir3.TabIndex = 27;
+            // 
+            // ListaBuscarContar
+            // 
+            this.ListaBuscarContar.FormattingEnabled = true;
+            this.ListaBuscarContar.Location = new System.Drawing.Point(331, 448);
+            this.ListaBuscarContar.Name = "ListaBuscarContar";
+            this.ListaBuscarContar.Size = new System.Drawing.Size(169, 82);
+            this.ListaBuscarContar.TabIndex = 28;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(918, 336);
+            this.ClientSize = new System.Drawing.Size(807, 559);
+            this.Controls.Add(this.ListaBuscarContar);
+            this.Controls.Add(this.TextoEscribir3);
+            this.Controls.Add(this.btnBuscarContar);
+            this.Controls.Add(this.ListaBuscarReemplazar);
+            this.Controls.Add(this.TextoEscribir2);
+            this.Controls.Add(this.btnBuscarReemplazar);
+            this.Controls.Add(this.ListaOrden2);
+            this.Controls.Add(this.btnOrden2);
+            this.Controls.Add(this.ListaContar);
+            this.Controls.Add(this.btnContar);
+            this.Controls.Add(this.ListaNoRepeticiones);
+            this.Controls.Add(this.btnAleatorioRepeticiones);
+            this.Controls.Add(this.ListaReemplazar);
+            this.Controls.Add(this.btnReemplazar);
+            this.Controls.Add(this.ListaInvertir);
+            this.Controls.Add(this.btnInvertir);
+            this.Controls.Add(this.TextoEscribir);
             this.Controls.Add(this.ListaBuscar);
-            this.Controls.Add(this.BotónBuscar);
+            this.Controls.Add(this.btnBuscar);
             this.Controls.Add(this.ListaOrden);
-            this.Controls.Add(this.BotónOrdenar);
+            this.Controls.Add(this.btnOrden);
             this.Controls.Add(this.ListaAritmetica);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnAritmetica);
             this.Controls.Add(this.ListaPosicion);
             this.Controls.Add(this.BtnPosiciones);
             this.Controls.Add(this.ListaMaxYMin);
@@ -169,7 +351,9 @@
             this.Controls.Add(this.BtnAleatorio);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -181,12 +365,29 @@
         private System.Windows.Forms.ListBox ListaMaxYMin;
         private System.Windows.Forms.Button BtnPosiciones;
         private System.Windows.Forms.ListBox ListaPosicion;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnAritmetica;
         private System.Windows.Forms.ListBox ListaAritmetica;
-        private System.Windows.Forms.Button BotónOrdenar;
+        private System.Windows.Forms.Button btnOrden;
         private System.Windows.Forms.ListBox ListaOrden;
-        private System.Windows.Forms.Button BotónBuscar;
+        private System.Windows.Forms.Button btnBuscar;
         private System.Windows.Forms.ListBox ListaBuscar;
+        private System.Windows.Forms.TextBox TextoEscribir;
+        private System.Windows.Forms.Button btnInvertir;
+        private System.Windows.Forms.ListBox ListaInvertir;
+        private System.Windows.Forms.Button btnReemplazar;
+        private System.Windows.Forms.ListBox ListaReemplazar;
+        private System.Windows.Forms.Button btnAleatorioRepeticiones;
+        private System.Windows.Forms.ListBox ListaNoRepeticiones;
+        private System.Windows.Forms.Button btnContar;
+        private System.Windows.Forms.ListBox ListaContar;
+        private System.Windows.Forms.Button btnOrden2;
+        private System.Windows.Forms.ListBox ListaOrden2;
+        private System.Windows.Forms.Button btnBuscarReemplazar;
+        private System.Windows.Forms.TextBox TextoEscribir2;
+        private System.Windows.Forms.ListBox ListaBuscarReemplazar;
+        private System.Windows.Forms.Button btnBuscarContar;
+        private System.Windows.Forms.TextBox TextoEscribir3;
+        private System.Windows.Forms.ListBox ListaBuscarContar;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -14,6 +14,7 @@ namespace WindowsFormsApplication1
     {
         Random aleatorio = new Random();
         int[] vector = new int[50];
+        int[] vector2 = new int[50];
 
         public Form1()
         {
@@ -37,6 +38,7 @@ namespace WindowsFormsApplication1
 
         private void BtnMayor_Click(object sender, EventArgs e)
         {
+            ListaMaxYMin.Items.Clear();
             int mayor = -1, menor = 101;
             for(int i = 0; i < vector.Length; i++)
             {
@@ -51,12 +53,14 @@ namespace WindowsFormsApplication1
 
         private void BtnPosiciones_Click(object sender, EventArgs e)
         {
+            ListaPosicion.Items.Clear();
             ListaPosicion.Items.Add("La posición 20 es " + vector[20]);
             ListaPosicion.Items.Add("La posición 30 es " + vector[30]);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            ListaAritmetica.Items.Clear();
             int acc = 0;
             for(int i = 0; i < vector.Length; i++)
             {
@@ -67,7 +71,8 @@ namespace WindowsFormsApplication1
 
         private void BotónOrdenar_Click(object sender, EventArgs e)
         {
-            for(int i = 0; i < vector.Length - 1; i++)
+            ListaOrden.Items.Clear();
+            for (int i = 0; i < vector.Length - 1; i++)
             {
                 for (int j = 0; j < vector.Length -1 - i; j++)
                     if (vector[j] > vector[j + 1])
@@ -83,6 +88,191 @@ namespace WindowsFormsApplication1
             {
                 ListaOrden.Items.Add(vector[i]);
             }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BotónBuscar_Click(object sender, EventArgs e)
+        {
+            ListaBuscar.Items.Clear();
+            int num = Convert.ToInt16(TextoEscribir.Text);
+            int acc = 0;
+
+            for(int i = 0; i < vector.Length; i++)
+            {
+                if(vector[i] == num)
+                {
+                    acc++;
+                }
+            }
+
+            ListaBuscar.Items.Add(acc);
+        }
+
+        private void BotonInvertir_Click(object sender, EventArgs e)
+        {
+            ListaInvertir.Items.Clear();
+            Array.Reverse(vector);
+
+            for(int i = 0; i < vector.Length; i++)
+            {
+                ListaInvertir.Items.Add(vector[i]);
+            }
+        }
+
+        private void btnReemplazar_Click(object sender, EventArgs e)
+        {
+            ListaReemplazar.Items.Clear();
+            Array.Sort(vector);
+            int num = 0;
+            for(int i = 0; i < vector.Length; i++)
+            {
+                if(vector[i] == num)
+                {
+                    vector[i] = -1;
+                }
+                else
+                {
+                    num = vector[i];
+                }
+                ListaReemplazar.Items.Add(vector[i]);
+            }
+            
+        }
+
+        private void btnAleatorioRepeticiones_Click(object sender, EventArgs e)
+        {
+            ListaNoRepeticiones.Items.Clear();
+            
+            for(int i = 0; i < vector2.Length; i++)
+            {
+                Boolean continuar = true;
+                
+                while (continuar)
+                {
+                    int num = aleatorio.Next(100);
+                    Boolean esta = false;
+                    for (int j = 0; j < i; j++)
+                    {
+                        if (vector2[j] == num)
+                        {
+                            esta = true;
+                            
+                        }
+                    }
+                    if(esta == false)
+                        continuar = false;
+                    vector2[i] = num;
+                }
+                
+                ListaNoRepeticiones.Items.Add(vector2[i]);
+            }
+        }
+
+        private void btnContar_Click(object sender, EventArgs e)
+        {
+            ListaContar.Items.Clear();
+            int mayores = 0, menores = 0;
+
+            for(int i = 0; i < vector2.Length; i++)
+            {
+                if(vector2[i] > 25)
+                {
+                    mayores++;
+                }
+                if (vector2[i] < 25)
+                {
+                    menores++;
+                }
+            }
+
+            ListaContar.Items.Add("Hay " + mayores + " números mayores de 25");
+            ListaContar.Items.Add("Hay " + menores + " números menores de 25");
+        }
+
+        private void btnOrdenar2_Click(object sender, EventArgs e)
+        {
+            ListaOrden2.Items.Clear();
+            for (int i = 0; i < vector2.Length - 1; i++)
+            {
+                for (int j = 0; j < vector2.Length - 1 - i; j++)
+                    if (vector2[j] > vector2[j + 1])
+                    {
+                        int aux;
+                        aux = vector2[j];
+                        vector2[j] = vector2[j + 1];
+                        vector2[j + 1] = aux;
+                    }
+
+            }
+            for (int i = 0; i < vector2.Length; i++)
+            {
+                ListaOrden2.Items.Add(vector2[i]);
+            }
+        }
+
+        private void btnBuscarReemplazar_Click(object sender, EventArgs e)
+        {
+            ListaBuscarReemplazar.Items.Clear();
+            int num = Convert.ToInt32(TextoEscribir2.Text);
+
+            for (int i = 0; i < vector2.Length; i++)
+            {
+                //Compruebo si está
+                if (vector2[i] == num)
+                {
+                    //Lo cambio por otro
+                    for (int j = 0; j < vector2.Length; j++)
+                    {
+                        Boolean continuar = true;
+
+                        while (continuar)
+                        {
+                            int num2 = aleatorio.Next(100);
+                            Boolean esta = false;
+                            for (int k = 0; k < j; k++)
+                            {
+                                if (vector2[k] == num2)
+                                {
+                                    esta = true;
+
+                                }
+                            }
+                            if (esta == false)
+                                continuar = false;
+                            vector2[i] = num2;
+                        }
+                    }
+                }
+                ListaBuscarReemplazar.Items.Add(vector2[i]);
+            }
+        }
+
+        private void btnBuscarContar_Click(object sender, EventArgs e)
+        {
+            ListaBuscarContar.Items.Clear();
+            int num = Convert.ToInt32(TextoEscribir3.Text);
+            int mayores = 0, menores = 0;
+
+            for (int i = 0; i < vector2.Length; i++)
+            {
+                if (vector2[i] > num)
+                {
+                    mayores++;
+                }
+                if (vector2[i] < num)
+                {
+                    menores++;
+                }
+            }
+
+            ListaBuscarContar.Items.Add("Hay " + mayores + " números mayores de " + num);
+            ListaBuscarContar.Items.Add("Hay " + menores + " números menores de " + num);
+
+
         }
     }
 }
